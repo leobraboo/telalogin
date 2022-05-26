626511b38587f8c1a0e60f31bbab652c7ff0a6a4
leobraboo <leonardo.l.souza@ufv.br>
2022-05-26 14:20:33 -0300

    rota para a tela home

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,10 @@
 const {Router} = require("express")
 const LoginRoute = require("./login")
+const HomeRouter = require("./home")
 
 const router = Router()
 
 router.use('/', LoginRoute)
-
+router.use('/home' ,HomeRouter )
 
 module.exports = router;
