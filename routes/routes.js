3deefff1abcf4af08d443ed9d330f3d93f2271fd
leobraboo <leonardo.l.souza@ufv.br>
2022-05-29 22:11:29 -0300

    rota clients

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,13 @@
 const {Router} = require("express")
 const LoginRoute = require("./login")
 const HomeRouter = require("./home")
+const ClienteRouter = require("./clientes")
+
 
 const router = Router()
 
+
+router.use('/clientes', ClienteRouter)
 router.use('/', LoginRoute)
 router.use('/home' ,HomeRouter )
 
