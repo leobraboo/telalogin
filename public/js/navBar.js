0ade9056b2ebdc2eab45045fa6d30cbdd7924c4c
leobraboo <leonardo.l.souza@ufv.br>
2022-06-01 02:39:40 -0300

    navbar ajustes

--- public/js/navBar.js
+++ public/js/navBar.js
@@ -1,21 +1,45 @@
-class menuNavBar {
-    constructor(menu , list , links){
-        this.menu = document.querySelector(menu);
-        this.list = document.querySelector(list);
-        this.links = document.querySelector(links);
-        this.activeClass = "active";
-
-        this.handleClick = this.handleClick.bind (this);
+class MenuNavbar {
+    constructor(menu, list, links) {
+      this.mobileMenu = document.querySelector(menu);
+      this.navList = document.querySelector(list);
+      this.navLinks = document.querySelectorAll(links);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
     }
-
-    animateLinks () {
-        this.links.forEach ((link , index) =>{
-            link.style.animation 
-                ? (link.style.animation = "")
-                : (link.style.animation = `navLinkFade 0.5s ease forwards $ {
-                    index / 7 + 0.3
-                }s`);
-        
-        });
+  
+    animateLinks() {
+      this.links.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
     }
-}
+  
+    handleClick() {
+      this.list.classList.toggle(this.activeClass);
+      this.menu.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.menu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.menu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const menuNavbar = new menuNavbar(
+    ".menu",
+    ".nav-list",
+    ".nav-list li",
+  );
+  menuNavbar.init();
+
